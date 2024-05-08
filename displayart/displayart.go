530c9911767edf47736138c6757a9432cb19a323
shfana <wambitafana@gmail.com>
2024-05-08 06:36:28 +0300

    Add README.md file

--- displayart/displayart.go
+++ displayart/displayart.go
@@ -1,62 +1,63 @@
 package asciiart
 
 import (
+	"fmt"
 	"strings"
+	"unicode"
 
 	asciiart "asciiart/mapfile"
 )
 
 func DisplayArt(filePath, input string) (string, error) {
-	var b strings.Builder // saves on memory
+	var buildStr strings.Builder // saves on memory
 	// fetch the map and handle any errors
-	m, err := asciiart.MapFile(filePath)
+	Asciimap, err := asciiart.MapFile(filePath)
+
+	if len(Asciimap) == 0 {
+		fmt.Println("Error: Character map is empty, please provide valid char map")
+	}
 	if err != nil {
-		return b.String(), err
+		return buildStr.String(), err
 	}
 	// counter for new lines or no of words split by new
 	count := 0
-	words := strings.Split(input, "\\n")
-	for _, word := range words {
+	words := strings.ReplaceAll(input, "\\n", "\n")
+	wordsSlice := strings.Split(words, "\n")
+
+	for _, word := range wordsSlice {
 		if word == "" {
 			count++
-			if count < len(words) {
-				b.WriteString("\n")
+			if count < len(wordsSlice) {
+				buildStr.WriteString("\n")
 			}
-			continue
-		}
-		// printing the art for each word/substring . Check if each char exists in the map
-		for i := 0; i < 8; i++ {
-			for _, c := range word {
-				s, ok := m[c]
-				if ok {
-					b.WriteString(s[i])
-				} else {
-					b.WriteString("?")
+		} else {
+			// printing the art for each word/substring . Check if each char exists in the map
+			for i := 0; i < 8; i++ {
+				for _, c := range word {
+					s, ok := Asciimap[c]
+					if ok {
+						buildStr.WriteString(s[i])
+					} else {
+						buildStr.WriteString("?")
+					}
+
 				}
-
+				buildStr.WriteString("\n") // print new line for the next line of art
 			}
-			b.WriteString("\n") // print new line for the next line of art
 		}
-
 	}
-	return b.String(), nil
+	return buildStr.String(), nil
 }
 
 func IsPrintable(input string) bool {
-	for i := 0; i < len(input); i++ {
-		c := input[i]
-		if !(c < ' ' || c > '~' || c == '\n') {
-			// Return false if the character is out of the basic printable range
-			return false
-		}
-		if c == '\\' {
-			if i+1 < len(input) && input[i+1] == '\\' {
-				// Skip the next character if it is a valid escaped backslash
-				i++ // Skip the escaped backslash
-			} else {
-				// If it's not a valid escape (like \n, \t, etc.), and also not another backslash, it's invalid
+	for i, val := range input {
+		if i < len(input)-1 && input[i] == '\\' {
+			if (input[i+1] == 't' || input[i+1] == 'a' || input[i+1] == 'b' || input[i+1] == 'v' || input[i+1] == 'f' || input[i+1] == 'r') && !(input[i+1] == 'n') {
 				return false
 			}
+		}
+		if !unicode.IsPrint(val) {
+			return false
 		}
 	}
 	return true
